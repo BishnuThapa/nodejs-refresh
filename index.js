a4e9a5b78278825f540ed6b740a371e4d30c928a
Bishnu <thapabishnu20@gmail.com>
2025-12-07 13:00:14 +0545

    getting started with expressjs

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const http = require('http');
-const fs = require('fs');
+// const fs = require('fs');
 
-const myServer = http.createServer((req, res) => { 
-    // const log = `${Date.now()}: New request received \n`;
-    const log = `${Date.now()}: ${req.url} New request received \n`;
-    fs.appendFile('log.txt',log, (err, data) => {
-        res.end("Hello From Server");
-    })
+// const myServer = http.createServer((req, res) => {
+//     // const log = `${Date.now()}: New request received \n`;
+//     const log = `${Date.now()}: ${req.url} New request received \n`;
+//     fs.appendFile('log.txt',log, (err, data) => {
+//         res.end("Hello From Server");
+//     })
+// });
+
+// myServer.listen(8000, () => console.log('Server is listening on port 8000'));
+
+
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    return res.send('Hello from Home Page');
 });
 
-myServer.listen(8000, () => console.log('Server is listening on port 8000'));
+app.get('/about', (req, res) => {
+    return res.send('Hello from About Page');
+});
+// const myServer = http.createServer(app);
+// myServer.listen(8000, () => console.log("Server is listening on port 8000"));
+
+app.listen(8000, () => console.log("Server is listening on port 8000"));
+
