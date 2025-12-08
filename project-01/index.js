e823dea76699b1140456b1cde6282647139169e3
Bishnu <thapabishnu20@gmail.com>
2025-12-08 09:27:13 +0545

    custom middleware to log user ip and req path to log.txt file

--- project-01/index.js
+++ project-01/index.js
@@ -1,18 +1,68 @@
-const express=require('express');
+const express = require('express');
+const fs=require('fs');
 const app=express();
 const PORT = 8000
-
 const users=require('./MOCK_DATA.json')
+
+app.use(express.json()); // middleware for post req body parsing
+// Express Middleware
+// app.use((req, res, next) => {
+//     console.log("Hello From Middleware 1"); // This holds the request until next() is called
+//     return res.json({ message: "Hello from Middleware 1" });
+//     next();
+// });
+
+app.use((req, res, next) => {
+  fs.appendFile(
+    "log.txt",
+    `\n${new Date().toISOString()} - ${req.ip} - ${req.method} - ${
+      req.originalUrl
+    }`,
+    (err) => {
+      if (err) {
+        console.error("Failed to write log:", err);
+      }
+      next();
+    }
+  );
+});
+
 
 app.get('/api/users', (req, res) => {
     res.json(users);
 });
 
-app.get('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find(u => u.id === userId);
-    res.json(user)
-});
+// Route Grouping for /api/users/:id
+app.route('/api/users/:id')
+    .get((req, res) => {
+        const id = parseInt(req.params.id);
+        const user = users.find((user) => user.id === id);
+        res.json(user)
+    })
+    .patch((req, res) => {
+        return res.json("PENDING")
+    })
+    .delete((req, res) => {
+        return res.json("PENDING")
+    });
+
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    // save to mock_data.json
+    users.push({ ...body, id: users.length + 1 });
+    fs.writeFile('MOCK_DATA.json', JSON.stringify(users), (err, data) => { 
+        return res.json({status:"success",id:users.length});
+    })
+})
+
+// app.patch('/api/users/:id', (req, res) => {
+//     return res.json("PENDING")
+// });
+
+// app.delete('/api/users/:id', (req, res) => {
+//     return res.json("PENDING")
+// });
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
